0184bae53e00c4d455f2b2865b78c0e38e4beef4
Walid Chouaibi <you@example.com>
2025-01-27 13:36:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# creatify-app" 
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Creatify!</h1>
+      <p>Your ultimate tool for content creation.</p>
+    </div>
+  );
+}
+
+export default App;
